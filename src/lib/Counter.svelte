ded8f8c33e74ba94a5949a2fa5587015e74b228a
sk0g <ani.mehta@mail.com>
2022-04-06 00:36:52 +1000

    Init

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,20 @@
+<script>
+  let count = 0;
+  const increment = () => {
+    count += 1;
+  };
+</script>
+
+<button class="bg-red-200 py-3 px-6 rounded" on:click={increment}>
+  Clicks: {count}
+</button>
+
+<style>
+  button:focus {
+    border: 2px solid #ff3e00;
+  }
+
+  button:active {
+    background-color: rgba(255, 62, 0, 0.2);
+  }
+</style>
