79fd47d03f673efb3ae29523998b5c8d9697cd4f
sk0g <ani.mehta@mail.com>
2022-04-07 21:10:56 +1000

    Make header always visible

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -14,7 +14,7 @@
     style:background-color={modeIsDebug
         ? "rgba(253, 187, 116, 0.6"
         : "rgba(94, 234, 212, 0.6)"}
-    class="bar flex place-content-center -mx-10"
+    class="flex place-content-center sticky top-0 -mx-7 z-50"
 >
     <button on:click={setDebug} class="px-2 hover:bg-orange-300">Debug</button>
     <h1 class="m-2">|</h1>
